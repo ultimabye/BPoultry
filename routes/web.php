a8e48ddc4618215fb07ca75a2018ad088764dfab
maaz56 <ultimabytes@gmail.com>
2023-04-08 03:49:59 +0500

    root path Changed

--- routes/web.php
+++ routes/web.php
@@ -17,21 +17,21 @@ use Illuminate\Support\Facades\Route;
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
+|Route::get('/', function () {
     return view('welcome');
 });
+*/
+Route::get('/', function () {
+    return view('index2');
+});
+
 Route::get('/contact', function () {
     return view('contact');
 });
 Route::get('/index', function () {
     return view('index');
 });
-Route::get('/index2', function () {
-    return view('index2');
-});
+
 Route::get('/addCustomer', function () {
     return view('addCustomer');
 });
